50a8c410af9269c919b6ac4c29dd58e3d878374f
Aaron Lafitte <aaron.lafitte@aggiemail.usu.edu>
2018-10-15 21:08:52 -0600

    Added the base classes and admin login

--- quizme/quizme/settings.py
+++ quizme/quizme/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'quizmeapp.apps.QuizmeappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
