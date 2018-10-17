2a1154ff4c62e58dc64add94f8e7d4253b6373c8
bradybod <bradybod@gmail.com>
2018-10-17 16:03:11 +0000

    api built still need to implement user api

--- quizme/quizme/urls.py
+++ quizme/quizme/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('quizmeapp.urls')),
     path('admin/', admin.site.urls),
+    path('user-auth/v1/', include('userAuthApi.urls')),
 ]
