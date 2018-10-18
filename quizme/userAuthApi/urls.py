aa9f204e5beeadd9eeb62152f288825b875c0085
Brady <bradybodily@Bradys-iMac.local>
2018-10-17 23:28:59 -0600

    fixed bug in userAuthAPI/urls.py

--- quizme/userAuthApi/urls.py
+++ quizme/userAuthApi/urls.py
@@ -3,5 +3,4 @@ from django.urls import include, path
 urlpatterns = [
     path('users/', include('users.urls')),
     path('rest-auth/', include('rest_auth.urls')),
-    url(r'^users/register', 'userAuthAPI.views.create_auth'),
 ]
