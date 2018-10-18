b27a65802762ba32f8f193a2ec4b6bed3bbc4b40
Brady <bradybodily@Bradys-iMac.local>
2018-10-17 23:24:48 -0600

    fixed bug in base.py

--- quizme/quizme/settings/base.py
+++ quizme/quizme/settings/base.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'quizme.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': str(ROOT_DIR.path('db.sqlite3')),
     }
 }
 
