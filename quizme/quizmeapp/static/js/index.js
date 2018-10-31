44f8771a8ff2d0d25ed39e6b407d9ed9f9aa9ae5
Matthew Shaw <matthewshaw53@yahoo.com>
2018-10-30 20:09:13 -0600

    Got react working for real

--- quizme/quizmeapp/static/js/index.js
+++ quizme/quizmeapp/static/js/index.js
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-function Welcome() {
-  return <h1>Hello, World!</h1>;
-}
+const element = (<h1>Hello, World!</h1>)
 
-const element = <Welcome name="world" />
+
 ReactDOM.render(
-  element,
-  document.getElementById('react')
+	element,
+  	document.getElementById('react')
 );
