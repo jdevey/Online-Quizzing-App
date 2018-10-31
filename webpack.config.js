676066a4ba25ab7aafc2e426393d86eafd855799
Matthew Shaw <matthewshaw53@yahoo.com>
2018-10-30 19:13:06 -0600

    working on babel, react, and webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  context: __dirname,
+
+  entry: './quizme/quizmeapp/static/js/index',
+
+  output: {
+      path: path.resolve('./quizme/quizmeapp/static/bundles/'),
+      filename: "[name]-[hash].js",
+  },
+
+  plugins: [
+    new BundleTracker({filename: './quizme/webpack-stats.json'}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+
+};
