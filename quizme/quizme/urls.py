4d5071058d1c1a95fa308a25a91a66fe38988635
Aaron Lafitte <aaron.lafitte@aggiemail.usu.edu>
2018-11-06 18:58:26 -0700

    Fixed Templates stuff

--- quizme/quizme/urls.py
+++ quizme/quizme/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('home', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('quizmeapp.urls')),
     path('api/v1/', include('API.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
