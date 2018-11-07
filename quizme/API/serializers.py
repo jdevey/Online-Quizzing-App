53a0f1893bc897531979938b705c78c588e4315f
bradybod <bradybod@gmail.com>
2018-11-07 13:11:49 -0700

    added is authenticated

--- quizme/API/serializers.py
+++ quizme/API/serializers.py
@@ -2,6 +2,7 @@
 from rest_framework import serializers
 from quizmeapp import models
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
--- quizme/API/views.py
+++ quizme/API/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from quizmeapp.models import User, Question
 from .serializers import UserSerializer
 
 # Create your views here.
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = UserSerializer
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = UserSerializer
